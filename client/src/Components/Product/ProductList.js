638466cca9fd014d80bfaa4e921baa17d3e99324
shohedul350 <shohedul350@gmail.com>
2020-10-05 01:52:08 +0600

    React data_grid added

--- client/src/Components/Product/ProductList.js
+++ client/src/Components/Product/ProductList.js
@@ -1,4 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react'
+import CircularProgress from '@material-ui/core/CircularProgress';
+import moment from 'moment'
 import {Link} from 'react-router-dom'
 
 import {
@@ -7,18 +9,20 @@ import {
 } from '@devexpress/dx-react-grid';
 import {
   Table,
-  Grid as tGrid,
   TableHeaderRow,
   TableFilterRow,
   VirtualTable,
   TableColumnResizing,
+  Grid,
 } from '@devexpress/dx-react-grid-material-ui';
 
 
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
+// import { Grid as MeterialGrid } from '@material-ui/core/Grid';
+import { Button} from '@material-ui/core';
+import Alert from '@material-ui/lab/Alert'
+
 import AddIcon from '@material-ui/icons/Add';
 
 import DeleteIcon from '@material-ui/icons/Delete';
@@ -46,14 +50,20 @@ const useStyles = makeStyles((theme) => ({
       
     },
     content:{
-       margin:10,
+       margin:8,
        flexGrow: 1,
+       textAlign: 'center',
     height: '77vh',
     overflow: 'auto',
     },
     linkStyle:{
       textDecoration: 'none',
       color: 'white'
+    },
+    spinner: {
+      textAlign: 'center',
+      marginTop:'17%'
+    
     }
   }))
  const ProductList = () => {
@@ -82,27 +92,34 @@ const useStyles = makeStyles((theme) => ({
     ]);
   
     const [columns] = useState([
+      { name: 'sl', title: 'Sl' },
       { name: 'name', title: 'Product name' },
       { name: 'image', title: 'Image' },
       { name: 'price', title: 'Price' },
       { name: 'sellingPrice', title: 'Selling Price' },
-      { name: 'stock', title: 'Current stock' },
+      { name: 'stock', title: 'Stock' },
       { name: 'unit', title: 'Unit' },
       { name: 'description', title: 'Description' },
       { name: 'createdAt', title: 'Date Added' },
       { name: 'action', title: 'Action', columnFilteringEnabled: false },
     ]);
   
-    const data = products.map((c) => {
+    const data = products.map((c,index) => {
       return {
+        sl: index+1,
         name: c.name,
-        image: 'sdjafk',
+        image: (<div> <img
+          src={`/${c.image}`}
+          style={{width:"2rem",height:"2rem"}}
+          className="img-fluid"
+          alt="product"
+          /></div>),
         price: c.price,
         sellingPrice: c.sellingPrice,
-        stock: c.stock,
+        stock:(c.stock >= 0 ? c.stock : <Alert severity="warning">Stock Out</Alert> ),
         unit: c.unit,
         description: c.description,
-        createdAt: c.createdAt,
+        createdAt:(moment( c.createdAt).format("MMMM Do YYYY")),
         action: (<div>
           <IconButton onClick={() => handleEdit(c._id)} aria-label="edit">
             <EditIcon />
@@ -115,31 +132,37 @@ const useStyles = makeStyles((theme) => ({
     })
   
     const [defaultColumnWidths] = useState([
+      { columnName: 'sl', width: 50 },
       { columnName: 'name', width: 150 },
-      { columnName: 'image', width: 150},
+      { columnName: 'image', width: '5rem'},
       { columnName: 'price', width: 100 },
       { columnName: 'sellingPrice', width: 100 },
-      { columnName: 'stock', width: 150 },
-      { columnName: 'unit', width: 150  },
+      { columnName: 'stock', width: 130 },
+      { columnName: 'unit', width: 80  },
       { columnName: 'description', width: 150  },
-      { columnName: 'createdAt', width: 250  },
-      { columnName: 'action', width: 240 },
+      { columnName: 'createdAt', width: 150  },
+      { columnName: 'action', width: 100 },
     ]);
 
     return (
         <div>
-        <Paper variant="outlined" square className={classes.tittle}>  
-          <Grid className={classes.addButton}>
+
+      {!products.length ? (<div className={classes.spinner}>
+        <CircularProgress size={80} />
+        </div>)
+         : (<div>
+          <Paper variant="outlined" square className={classes.tittle}>  
+          <div className={classes.addButton}>
             <Link to ='/dashboard/add-product' className={classes.linkStyle}>
                  <Button variant="contained" color="primary">
                  New Product<AddIcon/>
                 </Button>
           </Link>
-          </Grid>
+          </div>
            </Paper > 
            <Paper variant="outlined" elevation={5} className={classes.content}>
            <h1>All product here</h1>
-            <tGrid
+            <Grid
               rows={data}
               columns={columns}
             >
@@ -150,8 +173,14 @@ const useStyles = makeStyles((theme) => ({
               <TableColumnResizing defaultColumnWidths={defaultColumnWidths} />
               <TableHeaderRow />
               <TableFilterRow />
-            </tGrid>
+          
+            </Grid>
           </Paper> 
+          </div>
+
+         )
+        }
+      
         </div>
     )
 }
