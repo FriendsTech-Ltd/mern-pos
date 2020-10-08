36ca023a529d52c3cc7b35b3b624ff8495bbc4b1
Abdullah Naser <abdnaser.exe@gmail.com>
2020-10-08 17:32:32 +0600

    Customer searching added in create invocie

--- client/src/Components/Invoice/InvoiceCustomer.js
+++ client/src/Components/Invoice/InvoiceCustomer.js
@@ -59,11 +59,98 @@ const useStyles = makeStyles((theme) => ({
       // color: theme.palette.text.secondary,
     },
   input:{
-      
+    fontSize: '15px',
+    height: '30px',
+    boxSizing: 'border-box',
+    padding: '4px 6px',
+    width: 0,
+    minWidth: '30px',
+    flexGrow: 1,
+    border: 0,
+    margin: 0,
+    outline: 0,
+  },
+  listbox: {
 
   }
   }));
 
+  const InputWrapper = styled('div')`
+  width: 600px;
+  border: 1px solid #d9d9d9;
+  background-color: #fff;
+  border-radius: 4px;
+  padding: 1px;
+  display: flex;
+   flex-wrap: wrap;
+
+  &:hover {
+    border-color: #40a9ff;
+  }
+
+  &.focused {
+    border-color: #40a9ff;
+    box-shadow: 0 0 0 2px rgba(24, 144, 255, 0.2);
+  }
+
+  & input {
+    font-size: 15px;
+    height: 30px;
+    box-sizing: border-box;
+    padding: 4px 6px;
+    width: 0;
+    min-width: 30px;
+    flex-grow: 1;
+    border: 0;
+    margin: 0;
+    outline: 0;
+  }
+`;
+
+const Listbox = styled('ul')`
+  width: 300px;
+  margin: 2px 0 0;
+  padding: 0;
+  position: absolute;
+  list-style: none;
+  background-color: #fff;
+  overflow: auto;
+  max-height: 250px;
+  border-radius: 4px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.15);
+  z-index: 1;
+
+  & li {
+    padding: 5px 12px;
+    display: flex;
+
+    & span {
+      flex-grow: 1;
+    }
+
+    & svg {
+      color: transparent;
+    }
+  }
+
+  & li[aria-selected='true'] {
+    background-color: #fafafa;
+    font-weight: 600;
+
+    & svg {
+      color: #1890ff;
+    }
+  }
+
+  & li[data-focus='true'] {
+    background-color: #e6f7ff;
+    cursor: pointer;
+
+    & svg {
+      color: #000;
+    }
+  }
+`;
 
  
  const InvoiceCustomer = () => {
@@ -86,15 +173,17 @@ const useStyles = makeStyles((theme) => ({
    <Paper elevation={5} className={classes.customerSearch}>
    <div>
       <div {...getRootProps()} className={classes.searchIcon}>
-      <AddBoxIcon className={classes.iconSize}/> 
+      <AddBoxIcon className={classes.iconSize}/>
+      <InputWrapper>
         <input className={classes.input} {...getInputProps()} />
+      </InputWrapper>
       </div>
       {groupedOptions.length > 0 ? (
-        <ul className={classes.listbox} {...getListboxProps()}>
+        <Listbox {...getListboxProps()}>
           {groupedOptions.map((option, index) => (
             <li {...getOptionProps({ option, index })}>{option.name}</li>
           ))}
-        </ul>
+        </Listbox>
       ) : null}
     </div>
         </Paper>
