c2e37d4fa2179b6e7023d4bf223f1d07ef907e0e
shohedul350 <shohedul350@gmail.com>
2020-10-17 23:15:32 +0600

    added dashboard info component

---
+++ client/src/Components/Dashboard/DashboardInfo/index.js
@@ -0,0 +1,73 @@
+
+import React from 'react';
+import {
+  Container,
+  Grid,
+  makeStyles
+} from '@material-ui/core';
+import Budget from './Budget';
+import TasksProgress from './TasksProgress';
+import TotalProfit from './TotalProfit';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: theme.palette.background.dark,
+    minHeight: '100%',
+    paddingBottom: theme.spacing(3),
+    paddingTop: theme.spacing(3)
+  }
+}));
+
+const Dashboard = () => {
+  const classes = useStyles();
+
+  return (
+  
+      <Container maxWidth={false}>
+        <Grid
+          container
+          spacing={3}
+        >
+          <Grid
+            item
+            lg={3}
+            sm={6}
+            xl={3}
+            xs={12}
+          >
+            <Budget />
+          </Grid>
+          <Grid
+            item
+            lg={3}
+            sm={6}
+            xl={3}
+            xs={12}
+          >
+            <TotalCustomers />
+          </Grid>
+          <Grid
+            item
+            lg={3}
+            sm={6}
+            xl={3}
+            xs={12}
+          >
+            <TasksProgress />
+          </Grid>
+          <Grid
+            item
+            lg={3}
+            sm={6}
+            xl={3}
+            xs={12}
+          >
+            <TotalProfit />
+          </Grid>
+        </Grid>
+      </Container>
+
+  );
+};
+
+export default Dashboard;
